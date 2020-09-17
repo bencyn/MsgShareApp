c8d8a643d9d9b62995ad341398ecdf57f5f2959e
bencyn <bensonnjung39@gmail.com>
2020-09-17 13:29:16 +0300

    Section Two Video Four

--- app/src/main/java/com/example/msgshareapp/MainActivity.kt
+++ app/src/main/java/com/example/msgshareapp/MainActivity.kt
@@ -22,10 +22,21 @@ class MainActivity : AppCompatActivity() {
         btnSendMsgToNextActivity.setOnClickListener{
 
             val message: String = etUserMessage.text.toString()
-            Toast.makeText(this,message, Toast.LENGTH_SHORT).show()
-
+//            explicit intent
             val intent = Intent(this,SecondActivity::class.java)
+            intent.putExtra("user_message",message)
             startActivity(intent)
+        }
+
+        btnShare.setOnClickListener{
+            val message: String = etUserMessage.text.toString()
+            val intent = Intent()
+            intent.action = Intent.ACTION_SEND
+            intent.putExtra(Intent.EXTRA_TEXT, message)
+            intent.type = "text/plain"
+
+            startActivity(Intent.createChooser(intent, "Please select app: "))
+
         }
     }
 }
